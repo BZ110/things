3f8af06e2732e5a82de5a24c964f01211c06df25
Critical <49293139+BZ110@users.noreply.github.com>
2022-12-07 22:29:00 -0500

    Just saying...

--- Username-To-UUID-Minecraft.js
+++ Username-To-UUID-Minecraft.js
@@ -1,3 +1,7 @@
+// Mojang have a rate limit !!!!!
+// Please do not spam their servers, that's unfair.
+
+
 const MojangAPI = async function (username){
     const url = "https://api.mojang.com/users/profiles/minecraft/" + username;
     const response = await fetch(url);
